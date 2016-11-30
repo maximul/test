faa9bb2928136f6840bed3372046eb6793e715d9
maxim <maxim@exemple.ru>
2016-11-30 03:44:40 +0200

    Удалиили те же файлы + function.php

--- www/views/form.php
+++
@@ -1,7 +0,0 @@
-<form action="/add.php" method="post" enctype="multipart/form-data">
-    <label for="title">Название</label>
-    <input type="text" id="title" name="title"><br>
-    <label for="image">Файл</label>
-    <input type="file" id="image" name="image"><br>
-    <input type="submit">
-</form>
