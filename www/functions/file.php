f547e972c72e3674ce3f743f5346b0164c87e90d
maxim <maxim@exemple.ru>
2016-11-30 03:25:14 +0200

    Добавили файл add.php

---
+++ www/functions/file.php
@@ -0,0 +1,20 @@
+<?php
+
+function File_upload($field)
+{
+    if (empty($_FILES))
+        return false;
+
+    if (0 != $_FILES[$field]['error'])
+        return false;
+
+    if (is_uploaded_file($_FILES[$field]['tmp_name'])) {
+        $res = move_uploaded_file($_FILES[$field]['tmp_name'], __DIR__ . '/../img/' . iconv("UTF-8", "Windows-1251", $_FILES[$field]['name']));
+        if (!$res) {
+            return false;
+        } else {
+            return '/img/' . $_FILES[$field]['name'];
+        }
+    }
+    return false;
+}
